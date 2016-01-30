536f881fb36d1682717f48a5c51c7bdd4509993f
Dever4eg <Dever4eg@gmail.com>
2016-01-30 14:16:18 +0200

    remove bug git

--- Views/game/craft_view.php
+++
@@ -1,8 +0,0 @@
-<div class = "content_text"> 
-	<a class="but_reload" href="/game/craft" >
-	<br>  
-	<div class = "panel">
-		<h3 class = "center" >Мастерская</h3>
-	</div>
-	<br>
-</div>
--- Views/game/stock_view.php
+++
@@ -1,8 +0,0 @@
-<div class = "content_text"> 
-	<a class="but_reload" href="/game/stock" >
-	<br>  
-	<div class = "panel">
-		<h3 class = "center" >Склад</h3>
-	</div>
-	<br>
-</div>

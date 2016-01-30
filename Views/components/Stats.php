aff0f918e396ad0de715879ddcd6e31e3ae9a273
Dever4eg <Dever4eg@gmail.com>
2016-01-30 14:10:17 +0200

    Stats

---
+++ Views/components/Stats.php
@@ -0,0 +1,87 @@
+<p>
+    <span>
+        <a href = "/game/user" >
+            <span class = "span_img_head" >
+                <img src = "/resources/img/header_img/steave.png" >
+            </span>
+            <span class = "span_text_head" >
+                <?php echo $login ?>
+            </span>
+        </a>
+    </span>
+    <span>
+        (<a href="/game/logout">
+            выйти
+        </a>)
+    </span>
+    <span>
+        <a href = "/game" >
+            <span class = "span_img_head" >
+                &nbsp;<img src = "/resources/img/header_img/level.png" >
+            </span>
+            <span class = "span_text_head" >
+                <?php  ?>
+            </span>
+        </a>
+    </span>
+    <span>
+        <a href = "/game" >
+            <span class = "span_img_head" >
+                &nbsp;<img src = "/resources/img/header_img/energy.png" >
+            </span>
+            <span class = "span_text_head" >
+                <?php  ?>
+            </span>
+        </a>
+    </span>
+    <span>
+        <a href = "/game" >
+            <span class = "span_img_head" >
+                &nbsp;<img src = "/resources/img/header_img/heart.png" >
+            </span>
+            <span class = "span_text_head" >
+                <?php  ?>
+            </span>
+        </a>
+    </span>
+    <span>
+        <a href = "/game" >
+            <span class = "span_img_head" >
+                &nbsp;<img src = "/resources/img/header_img/credit.png" >
+            </span>
+            <span class = "span_text_head" >
+                <?php  ?>
+            </span>
+        </a>
+    </span>
+    <span>
+        <a href = "/game" >
+            <span class = "span_img_head" >
+                &nbsp;<img src = "/resources/img/header_img/lapis.png" >
+            </span>
+            <span class = "span_text_head" >
+                <?php  ?>
+            </span>
+        </a>
+    </span>
+    <span>
+        <a href = "/game" >
+            <span class = "span_img_head" >
+                &nbsp;<img src = "/resources/img/header_img/emerald.png" >
+            </span>
+            <span class = "span_text_head" >
+                <?php  ?>
+            </span>
+        </a>
+    </span>
+    <span>
+        <a href = "/game/stock" >
+            <span class = "span_img_head" >
+                &nbsp;<img src = "/resources/img/header_img/stock.png" >
+            </span>
+            <span class = "span_text_head" >
+                <?php  ?>
+            </span>
+        </a>
+    </span>
+</p>

e91d554dd18a89ec1d537aa02683978bb7040262
Dever4eg <Dever4eg@gmail.com>
2016-01-30 14:16:37 +0200

    remove bug git

---
+++ Views/game/farmer_view.php
@@ -0,0 +1,33 @@
+<div class = "content_text">
+	<a class="but_reload" href="/game/farmer" >
+	<br><div class = "panel">
+		<h3 class = "center" >Ферма</h3>
+	</div>
+	<br>
+	<a class = "know_base_a" href = "know_base/basic">
+		<ul>
+			<li class = "but_list_game" >
+				<p class = "greasy_text"><img src="/resourses/img/home_icon/fence.png">&nbsp;Огород</p>	
+			</li>
+		</ul>		
+	</a>
+	<a class = know_base_a href = "know_base/skills">
+		<ul>
+			<li class = "but_list_game" >
+				<p class = "greasy_text"><img src="/resourses/img/home_icon/paw.png">&nbsp;Загон</p>	
+			</li>
+		</ul>	
+	</a>
+	
+					
+			</li>
+		</ul>		
+	</a>
+		
+		</ul>	
+	</a>
+	
+		</ul>
+	</a>
+	<br>
+</div>
---
+++ Views/game/hike_view.php
@@ -0,0 +1,8 @@
+<div class = "content_text"> 
+	<a class="but_reload" href="/game/hike" >
+	<br>  
+	<div class = "panel">
+		<h3 class = "center" >Поход</h3>
+	</div>
+	<br>
+</div>
